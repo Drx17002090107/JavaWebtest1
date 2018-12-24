d1ea7c9a0a019f796d14c003303ee05681f7d421
Administrator <850167592@qq.com>
2018-12-24 09:44:37 +0800

    1219

---
+++ src/com/wzy/servlet/class12/LoginDBServlet.java
@@ -0,0 +1,69 @@
+package com.wzy.servlet.class12;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet(name = "LoginDBServlet",urlPatterns = "/LoginDBServlet")
+public class LoginDBServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Statement stmt=null;
+        ResultSet rs=null;
+        Connection conn=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/javawebclassdb1701";
+            String username="root";
+            String password="root";
+            conn=DriverManager.getConnection(url,username,password);
+            stmt=conn.createStatement();
+            String sql="select * from users";
+            rs=stmt.executeQuery(sql);
+            System.out.println("id | name | password | sex | age | birthday");
+            while (rs.next()){
+                int id=rs.getInt("id");
+                String name=rs.getString("name");
+                String psw=rs.getString("password");
+                String sex=rs.getString("sex");
+                int age=rs.getInt("age");
+                Date birthday=rs.getDate("birthday");
+                System.out.println(id+" | "+name+" | "+psw+" | "+sex+" | "+age+" | "+birthday);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(rs!=null){
+                try{
+                    rs.close();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                rs=null;
+            }
+            if(stmt!=null){
+                try {
+                    stmt.close();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                stmt=null;
+            }
+            if (conn!=null){
+                try{
+                    conn.close();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                conn=null;
+            }
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
